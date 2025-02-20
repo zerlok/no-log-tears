75d52dd4a2436e2961c6e4cf8375ce51fb149fd6
zerlok <danil.troshnev@gmail.com>
2025-02-20 19:05:32 +0100

    expose formatter classes in __all__ for public API

--- src/no_log_tears/formatter/__init__.py
+++ src/no_log_tears/formatter/__init__.py
@@ -1,1 +1,13 @@
 """Provides `logging.Formatter` implementations."""
+
+__all__ = [
+    "ISO8601DatetimeFormatter",
+    "JSONFormatter",
+    "SoftFormatter",
+    "TracebackFormatter",
+]
+
+from no_log_tears.formatter.datetime import ISO8601DatetimeFormatter
+from no_log_tears.formatter.json import JSONFormatter
+from no_log_tears.formatter.soft import SoftFormatter
+from no_log_tears.formatter.traceback import TracebackFormatter
